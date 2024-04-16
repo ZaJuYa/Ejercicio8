7896702e817edc2a66ef51b3290bae4acaa906d1
Ibai <ibai.saenzdeburuaga@ikasle.egibide.org>
2024-04-16 13:45:11 +0200

    Update usuarios

--- Ejercicio8/src/Modelo/Usuario.java
+++ Ejercicio8/src/Modelo/Usuario.java
@@ -5,5 +5,33 @@ public class Usuario {
     private String apellido;
     private String dni;
 
-    // Constructor, getters y setters
+    public Usuario(String nombre, String apellido, String dni) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
 }
